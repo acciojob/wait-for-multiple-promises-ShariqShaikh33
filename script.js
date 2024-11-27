d84d2324b73753645dc5ff3c74a51b856a700628
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2024-11-27 16:44:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,53 +1,63 @@
 let tablebody = document.getElementById('output');
 
-let row1 = tablebody.insertRow(-1);
-row1.setAttribute("id","Loading");
+let row = tablebody.insertRow(-1);
+row.setAttribute("id","Loading");
+let cell = row.insertCell(0);
+cell.setAttribute("colspan","2");
+cell.innerHTML = "Loading...";
 
-let cell1 = row1.insertCell(0);
-cell1.innerHTML = "Loading..."
-let cell2 = row1.insertCell(1);
-cell2.innerHTML = "Loading...";
-
+let startTime = Date.now();
 let Promise1 = new Promise((resolve) => {
+	let time1 = Math.random()*(3000-1000)+1000;
 	setTimeout(()=>{
-		resolve("Promise1");
-	},2000);
+		let row1 = tablebody.insertRow(-1);
+		let cell1 = row1.insertCell(0);
+		cell1.innerHTML = "Promise 1"
+		let cell2 = row1.insertCell(1);
+		cell2.innerHTML = (time1/1000).toFixed(1);
+		resolve(time1);
+	},time1);
+	
 });
 
 let Promise2 = new Promise((resolve) => {
+	let time2 = Math.random()*(3000-1000)+1000;
 	setTimeout(()=>{
-		resolve("Promise2");
-	},1000);
+		let row2 = tablebody.insertRow(-1);
+		let cell3 = row2.insertCell(0);
+		cell3.innerHTML = "Promise 2";
+		let cell4 = row2.insertCell(1);
+		cell4.innerHTML = (time2/1000).toFixed(1);
+		resolve(time2);
+	},time2);
 });
 
 let Promise3 = new Promise((resolve) => {
+	let time3 = Math.random()*(3000-1000)+1000;
 	setTimeout(()=>{
-		resolve("Promise3");
-	},3000);
+		let row3 = tablebody.insertRow(-1);
+		let cell5 = row3.insertCell(0);
+		cell5.innerHTML = "Promise 3";
+		let cell6 = row3.insertCell(1);
+		cell6.innerHTML = (time3/1000).toFixed(1);
+		resolve(time3);
+	},time3);
 });
 
 Promise.all([Promise1, Promise2, Promise3]).then(res => {
-	cell1.innerHTML = "Promise 1";
-	cell2.innerHTML = 2;
-	
-	let row2 = tablebody.insertRow(-1);
-	row2.classList.add("Loading");
-	let cell3 = row2.insertCell(0);
-	cell3.innerHTML = "Promise 2";
-	let cell4 = row2.insertCell(1);
-	cell4.innerHTML = 1;
+	let endTime = Date.now();
+	let TotalTime = (endTime - startTime) / 1000;
 
-	let row3 = tablebody.insertRow(-1);
-	row3.classList.add("Loading");
-	let cell5 = row3.insertCell(0);
-	cell5.innerHTML = "Promise 3";
-	let cell6 = row3.insertCell(1);
-	cell6.innerHTML = 3;
+	
+
+	
+
+	
 
 	let row4 = tablebody.insertRow(-1);
 	let cell7 = row4.insertCell(0);
 	cell7.innerHTML = "Total";
 	let cell8 = row4.insertCell(1);
-	cell8.innerHTML = 3.006;
+	cell8.innerHTML =TotalTime;
 })
 
