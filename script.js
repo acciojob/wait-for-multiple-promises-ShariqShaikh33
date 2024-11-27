952961715ec307dac341a3c82c05e8b1b44b7edc
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2024-11-27 17:07:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,16 +2,11 @@ let startTime = Date.now();
 
 let tablebody = document.getElementById('output');
 
-let row = tablebody.insertRow(-1);
-row.setAttribute("id","Loading");
-let cell = row.insertCell(0);
-cell.setAttribute("colspan","2");
-cell.innerHTML = "Loading...";
-
 let row1 = tablebody.insertRow(-1);
-		let cell1 = row1.insertCell(0);
-		cell1.innerHTML = "Promise 1"
-		let cell2 = row1.insertCell(1);
+row1.setAttribute("id","Loading");
+let cell1 = row1.insertCell(0);
+cell1.setAttribute("colspan","2");
+cell1.innerHTML = "Loading...";
 
 let row2 = tablebody.insertRow(-1);
 		let cell3 = row2.insertCell(0);
@@ -27,6 +22,9 @@ let row3 = tablebody.insertRow(-1);
 let Promise1 = new Promise((resolve) => {
 	let time1 = Math.random()*(3000-1000)+1000;
 	setTimeout(()=>{
+		cell1.innerHTML="Promise 1";
+		cell1.setAttribute("colspan","0");
+		let cell2 = row1.insertCell(1);
 		cell2.innerHTML = (time1/1000).toFixed(1);
 		resolve(time1);
 	},time1);
@@ -36,7 +34,6 @@ let Promise1 = new Promise((resolve) => {
 let Promise2 = new Promise((resolve) => {
 	let time2 = Math.random()*(3000-1000)+1000;
 	setTimeout(()=>{
-		
 		cell4.innerHTML = (time2/1000).toFixed(1);
 		resolve(time2);
 	},time2);
@@ -45,7 +42,6 @@ let Promise2 = new Promise((resolve) => {
 let Promise3 = new Promise((resolve) => {
 	let time3 = Math.random()*(3000-1000)+1000;
 	setTimeout(()=>{
-		
 		cell6.innerHTML = (time3/1000).toFixed(1);
 		resolve(time3);
 	},time3);
@@ -55,7 +51,7 @@ Promise.all([Promise1, Promise2, Promise3]).then(res => {
 	let endTime = Date.now();
 	let TotalTime = (endTime - startTime) / 1000;
 
-	document.getElementById("Loading").remove();
+
 	let row4 = tablebody.insertRow(-1);
 	let cell7 = row4.insertCell(0);
 	cell7.innerHTML = "Total";
