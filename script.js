ef75dcff2ee164672198641819c0508270c79fa8
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2024-11-27 15:49:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let tablebody = document.getElementById('output');
 
 let row1 = tablebody.insertRow(-1);
+row1.classList.add("Loading");
+
 let cell1 = row1.insertCell(0);
 cell1.innerHTML = "Loading..."
 let cell2 = row1.insertCell(1);
@@ -29,12 +31,14 @@ Promise.all([Promise1, Promise2, Promise3]).then(res => {
 	cell2.innerHTML = 2;
 	
 	let row2 = tablebody.insertRow(-1);
+	row2.classList.add("Loading");
 	let cell3 = row2.insertCell(0);
 	cell3.innerHTML = "Promise 2";
 	let cell4 = row2.insertCell(1);
 	cell4.innerHTML = 1;
 
 	let row3 = tablebody.insertRow(-1);
+	row3.classList.add("Loading");
 	let cell5 = row3.insertCell(0);
 	cell5.innerHTML = "Promise 3";
 	let cell6 = row3.insertCell(1);
